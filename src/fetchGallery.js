28431a5fed4aec85378b873d39f1920e0a5fb317
Roman Serediuk <box4rom@gmail.com>
2022-08-17 16:29:25 +0300

    styles.css create

--- src/fetchGallery.js
+++ src/fetchGallery.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
 async function fetchGallery(q, page) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '29319280-fde4903173ec234f4d94cddfd';
-  const URL = `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+    const perPage = 40;
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '29319280-fde4903173ec234f4d94cddfd';
+    const URL = `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
-  const response = await axios.get(URL);
-  return response;
+    const response = await axios.get(URL);
+    return response;
 }
 
 export { fetchGallery };
+
+// const fetchImages = async (searching, page) => {
+//     try {
+//         const response = await axios.get(
+//             `https://pixabay.com/api/?key=${API_KEY}&q=${searching}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+//         );
+//         console.log(response);
+//         return response.data;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
-// import axios from 'axios';
+import './css/styles.css';
 import Notiflix from 'notiflix';
 import { fetchGallery } from './fetchGallery.js';
 
 const refs = {
-  form: document.querySelector('#search-form'),
-  gallery: document.querySelector('.gallery'),
-  loadBtn: document.querySelector('.load-more'),
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadBtn: document.querySelector('.load-more'),
 };
-let page = 1;
+let page;
 let clientQuery = '';
+refs.loadBtn.classList.add('js__is-hidden');
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit(evt) {
-  evt.preventDefault();
-  refs.gallery.innerHTML = '';
-  //   console.log(evt);
-  //   console.log(evt.target.searchQuery.value);
+    evt.preventDefault();
+    page = 1;
+    refs.gallery.innerHTML = '';
 
-  try {
-    clientQuery = evt.target.searchQuery.value;
-    const data = await fetchGallery(clientQuery, page);
-    const dataArr = data.data.hits;
-    if (dataArr.length === 0 || clientQuery === '') {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      refs.form.reset();
-      return;
+    try {
+        clientQuery = evt.target.searchQuery.value;
+        const data = await fetchGallery(clientQuery, page);
+        const dataArr = data.data.hits;
+        if (dataArr.length === 0 || clientQuery === '') {
+            Notiflix.Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again.',
+            );
+            refs.form.reset();
+            return;
+        }
+
+        const totalImages = data.data.total;
+        Notiflix.Notify.success(`Hooray! We found ${totalImages} images.`);
+        createMarkup(dataArr);
+        refs.loadBtn.classList.remove('js__is-hidden');
+    } catch (error) {
+        error => console.log(error.message);
     }
-
-    const totalImages = data.data.total;
-    Notiflix.Notify.success(`Hooray! We found ${totalImages} images.`);
-    createMarkup(dataArr);
-  } catch (error) {
-    alert('ERROR');
-  }
 }
 
 // fetchGallery('car', 1).then(data => {
@@ -47,27 +48,36 @@ async function onFormSubmit(evt) {
 refs.loadBtn.addEventListener('click', onLoadBtnClick);
 
 async function onLoadBtnClick(evt) {
-  try {
-    page += 1;
-    const data = await fetchGallery(clientQuery, page);
-    const dataArr = data.data.hits;
-    createMarkup(dataArr);
-  } catch (error) {}
+    try {
+        page += 1;
+        const data = await fetchGallery(clientQuery, page);
+        const dataArr = data.data.hits;
+        createMarkup(dataArr);
+
+        if (data.data.totalHits < page * perPge) {
+            Notiflix.Notify.warning(
+                "We're sorry, but you've reached the end of search results.",
+            );
+            refs.loadBtn.classList.add('js__is-hidden');
+        }
+    } catch (error) {
+        error => console.log(error.message);
+    }
 }
 
 function parseMarkup(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    return arr
+        .map(
+            ({
+                webformatURL,
+                largeImageURL,
+                tags,
+                likes,
+                views,
+                comments,
+                downloads,
+            }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -86,11 +96,11 @@ function parseMarkup(arr) {
       ${downloads}
     </p>
   </div>
-</div>`
-    )
-    .join('');
+</div>`,
+        )
+        .join('');
 }
 
 function createMarkup(arr) {
-  refs.gallery.insertAdjacentHTML('beforeend', parseMarkup(arr));
+    refs.gallery.insertAdjacentHTML('beforeend', parseMarkup(arr));
 }
