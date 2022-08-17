65609d83a0ff83579871555ed1bb42ec1830ab24
Roman Serediuk <box4rom@gmail.com>
2022-08-17 21:16:42 +0300

    infinite scroll initiated

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const refs = {
     form: document.querySelector('#search-form'),
     gallery: document.querySelector('.gallery'),
     loadBtn: document.querySelector('.load-more'),
+    sentinel: document.querySelector('.js-sentinel'),
 };
 
 let page;
@@ -24,7 +25,9 @@ async function onFormSubmit(evt) {
     try {
         clientQuery = evt.target.searchQuery.value;
         const data = await fetchGallery(clientQuery, page);
+        // console.log(data);
         const dataArr = data.data.hits;
+        observer.observe(refs.sentinel);
         if (dataArr.length === 0 || clientQuery === '') {
             refs.loadBtn.classList.add('js__is-hidden');
             Notiflix.Notify.failure(
@@ -34,35 +37,67 @@ async function onFormSubmit(evt) {
             return;
         }
 
-        const totalImages = data.data.total;
+        const totalImages = data.data.totalHits;
         Notiflix.Notify.success(`Hooray! We found ${totalImages} images.`);
         createMarkup(dataArr);
-        refs.loadBtn.classList.remove('js__is-hidden');
+        // refs.loadBtn.classList.remove('js__is-hidden');
     } catch (error) {
         error => console.log(error.message);
     }
 }
+//////////// LoadMore Bottun Variant  /////////////////////////////////////////////
+// refs.loadBtn.addEventListener('click', onLoadBtnClick);
 
-refs.loadBtn.addEventListener('click', onLoadBtnClick);
+// async function onLoadBtnClick(evt) {
+//     try {
+//         page += 1;
+//         const data = await fetchGallery(clientQuery, page);
+//         const dataArr = data.data.hits;
+//         createMarkup(dataArr);
+//         observer.observe(refs.sentinel);
 
-async function onLoadBtnClick(evt) {
-    try {
-        page += 1;
-        const data = await fetchGallery(clientQuery, page);
-        const dataArr = data.data.hits;
-        createMarkup(dataArr);
-        // lightbox.refresh();
+//         if (data.data.totalHits < page * 40) {
+//             Notiflix.Notify.warning(
+//                 "We're sorry, but you've reached the end of search results.",
+//             );
+//             refs.loadBtn.classList.add('js__is-hidden');
+//             refs.form.reset();
+//         }
+//     } catch (error) {
+//         error => console.log(error.message);
+//     }
+// }
+//////////////////////////////////////////////////////////////////////////////////
 
-        if (data.data.totalHits < page * 40) {
-            Notiflix.Notify.warning(
-                "We're sorry, but you've reached the end of search results.",
-            );
-            refs.loadBtn.classList.add('js__is-hidden');
-            refs.form.reset();
-        }
-    } catch (error) {
-        error => console.log(error.message);
+const options = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1,
+};
+const observer = new IntersectionObserver(updateGallery, options);
+
+function updateGallery(entries) {
+    if (clientQuery === '') {
+        return;
     }
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            page += 1;
+            fetchGallery(clientQuery, page)
+                .then(data => {
+                    const dataArr = data.data.hits;
+                    createMarkup(dataArr);
+                    if (data.data.totalHits < page * 40) {
+                        Notiflix.Notify.warning(
+                            "We're sorry, but you've reached the end of search results.",
+                        );
+                        refs.form.reset();
+                        clientQuery = '';
+                    }
+                })
+                .catch(error => console.log(error.message));
+        }
+    });
 }
 
 function parseMarkup(arr) {
@@ -76,22 +111,20 @@ function parseMarkup(arr) {
                 views,
                 comments,
                 downloads,
-            }) => `<div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/>
-  <div class="info">
-  <p class="info-item">
-  <b>Likes</b>
-  ${likes}
-  </p>
-  <p class="info-item">
-  <b>Views</b>
-  ${views}
-  </p>
-  <p class="info-item">
-  <b>Comments</b>
-  ${comments}
-  </p>
-  <p class="info-item">
+            }) => `<div class="photo-card"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/>                            <div class="info">
+                            <p class="info-item">
+                            <b>Likes</b>
+                            ${likes}
+                            </p>
+                            <p class="info-item">
+                            <b>Views</b>
+                            ${views}
+                            </p>
+                            <p class="info-item">
+                            <b>Comments</b>
+                            ${comments}
+                            </p>
+                            <p class="info-item">
   <b>Downloads</b>
   ${downloads}
   </p>
